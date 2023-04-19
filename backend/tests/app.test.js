bc49548dae39cc8cdb45af8d593db8b65f77b94d
Adeola Idowu <adeolaidowu27@gmail.com>
2023-04-19 17:41:36 +0100

    add test for decode endpoint

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -61,3 +61,39 @@ describe("POST /encode", () => {
     })
 })
 
+describe("GET /decode", () => {
+
+    const db = {
+        "G2uLI34PKfZnTdkRp6js_": {
+            origUrl: "https://facebook.com",
+            shortUrl: "http://localhost:8080/G2uLI34PKfZnTdkRp6js_",
+            numberOfVisits: 0,
+            createdAt: new Date(),
+        }
+    }
+
+    describe("given a valid short url", () => {
+        const urlData = {
+            shortUrl: "G2uLI34PKfZnTdkRp6js_"
+        }
+        const response = db[urlData.shortUrl]
+        test("should respond with original url", () => {
+            expect(response.origUrl).toBe("https://facebook.com")
+        })
+
+        test("should specify json in the content type header", async () => {
+            const response = await request(app).get("/api/url/decode").send(urlData)
+            expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"))
+        })
+    })
+
+    describe("given an invalid short url", () => {
+        const urlData = {
+            shortUrl: "G2uLI34PKfZnTdkRp6xyz_"
+        }
+        const response = db[urlData.shortUrl]
+        test("should respond with a 400 status code", () => {
+            expect(response).toBeUndefined()
+        })
+    })
+})
